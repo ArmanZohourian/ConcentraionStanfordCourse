ae81a1f5a94b75350d6e278feaf06751ace01ef4
Arman <arman.zh1138@gmail.com>
2021-04-24 21:39:28 +0430

    Initial Commit

---
+++ ConcentrationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConcentrationSwiftUI
+//
+//  Created by Arman on 4/24/21.
+//  Copyright © 2021 Arman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
